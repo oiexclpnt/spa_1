ce6c93200afa793118689ea4675c3227c2c49ab2
Влад Мананков <iiioom01@buk.local>
2024-05-23 01:32:08 +0300

    first commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,17 @@
+function Contact() {
+    return (
+        <div className="">
+            <h1>Contact page</h1>
+            <div className="Cont-card">
+                <img src="..img/img-1.jpg" alt="" />
+                <div className="Cont-info">
+                    <h4>Ribaaa</h4>
+                    <h6>Address: Moscow, Pushkin st.45</h6>
+                    <h6>Time: 12:00 - 22:00</h6>
+                </div>
+            </div> 
+        </div>
+        )
+}
+
+export { Contact };
